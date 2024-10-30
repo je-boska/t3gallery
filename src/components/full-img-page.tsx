824e86edc14a5fdedcf380960397ccb46444da17
je-boska <joneirikboska@gmail.com>
2024-10-30 12:29:56 +0100

    Start styling image viewer

--- src/components/full-img-page.tsx
+++ src/components/full-img-page.tsx
@@ -3,5 +3,18 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
 
-  return <img src={image.url} alt={image.name} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0 bg-green-300">
+      <div className="h-full flex-shrink">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="max-h-full object-contain"
+        />
+      </div>
+      <div className="flex w-96 flex-shrink-0 flex-col">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
