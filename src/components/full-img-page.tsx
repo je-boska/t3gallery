ffc58983d091bb7b660b455457e8fdd89c4b0a52
je-boska <joneirikboska@gmail.com>
2024-11-07 15:40:01 +0100

    Center image

--- src/components/full-img-page.tsx
+++ src/components/full-img-page.tsx
@@ -4,15 +4,15 @@ export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
 
   return (
-    <div className="flex h-full w-full min-w-0 bg-green-300">
-      <div className="h-full flex-shrink">
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex h-full flex-shrink items-center justify-center">
         <img
           src={image.url}
           alt={image.name}
           className="max-h-full object-contain"
         />
       </div>
-      <div className="flex w-96 flex-shrink-0 flex-col">
+      <div className="flex w-96 flex-shrink-0 flex-col border-l">
         <div className="text-xl font-bold">{image.name}</div>
       </div>
     </div>
