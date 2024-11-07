48cc8388a125651cbf4aa8f3fe5845db25a2fddf
je-boska <joneirikboska@gmail.com>
2024-11-07 16:08:10 +0100

    Add metadata to image view

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
  - [x] "taint" (server-only)
  - [x] Use next-image component
  - [x] Error management (w/ Sentry)
- - [ ] Routing/image page (parallel route)
+ - [x] Routing/image page (parallel route)
  - [ ] Delete button (w/ server actions)
  - [ ] Analytics (Posthog)
  - [ ] Rate limiting (Upstash)
--- src/components/full-img-page.tsx
+++ src/components/full-img-page.tsx
@@ -1,7 +1,11 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const client = await clerkClient();
+
+  const uploaderInfo = await client.users.getUser(image.userId);
 
   return (
     <div className="flex h-full w-full min-w-0">
@@ -13,7 +17,15 @@ export default async function FullPageImageView(props: { id: number }) {
         />
       </div>
       <div className="flex w-96 flex-shrink-0 flex-col border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded by:</span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created on:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
